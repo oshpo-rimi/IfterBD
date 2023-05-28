82179f0c74b0c0ced4ae2c3c4cb47a2b63f500e1
Rimi Barua <rimi1998a@gmail.com>
2023-05-28 12:41:24 +0600

    The_ifter_meal_BD
    In this project, I created a food catalogue using HTML, CSS, React using TheMealDB API. The application allows for one to view all the meals, search for a meal basing on the available categories, and view all the ingredients used to prepare the meal.

---
+++ App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import MealCard from './MealCard';
+
+function App() {
+
+  const [meals, setMeals] = useState([])
+  const [mealName, setMealName] = useState("");
+
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+    .then(res =>res.json())
+    .then(data => {
+     setMeals(data.meals);
+    })
+  },[mealName]); 
+
+  const search = (event) => {
+    console.log(event.target.value);
+    setMealName(event.target.value);
+  }
+  return (
+    <div className="App">
+      <div className="px-[100px] mt-[50px]">
+      <input 
+      onChange={search}
+      type="text" placeholder="Search here your food" className="input input-bordered w-full" />
+      </div>
+   <div className= "grid grid-cols-3 gap-5 p-20">
+      {meals?.map((meal) => (
+        <MealCard meal={meal}></MealCard>
+      ))}
+   </div>
+    </div>
+  );
+}
+
+export default App;
